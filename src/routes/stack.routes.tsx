c34c6777dde940c3e8f095280bc74c1df62a87c1
marlloncampos <marllondcsp@gmail.com>
2021-04-22 19:49:45 -0300

    Criado uma nova rota para a página de PlantSave

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Confirmation } from '../pages/Confimation';
 
 import colors from '../styles/colors'
 import { PlantSelect } from '../pages/PlantSelect';
+import { PlantSave } from '../pages/PlantSave';
 
 const stackRoutes = createStackNavigator();
 
@@ -38,6 +39,8 @@ const AppRoutes: React.FC = () => (
         />
 
         <stackRoutes.Screen name="PlantSelect" component={PlantSelect}/>
+
+        <stackRoutes.Screen name="PlantSave" component={PlantSave} />
 
     </stackRoutes.Navigator>
 )
