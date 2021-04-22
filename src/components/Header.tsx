bb94b2f86768e6c82912f484fcd366e0b8925487
marlloncampos <marllondcsp@gmail.com>
2021-04-22 12:06:28 -0300

    Adicionado funcionalidade de captura de nome do usuário no AsyncStorage e mostrado em tela

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 import {  Text, Image, StyleSheet, View } from 'react-native'
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import userImg from '../assets/marllon.png'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
-
+import AsyncStorage from '@react-native-async-storage/async-storage'
 export function Header() {
+    const [userName,setUserName] = useState<string>();
+
+
+    async function getUserNameFromAsyncStorage() {
+        const user = await AsyncStorage.getItem('@plantmanager:user');
+        setUserName(user || '');
+    }
+
+    
+    useEffect(() => {
+        getUserNameFromAsyncStorage();
+    },[userName])
     return (
         <View style={styles.container}>
 
             <View>
                 <Text style={styles.greeting}>Olá</Text>
-                <Text style={styles.username}>Marllon</Text>
+                <Text style={styles.username}>{userName}</Text>
             </View>
 
             <Image source={userImg} style={styles.image}/>
